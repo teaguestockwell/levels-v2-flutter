3a19b89298ba41bb01ff7c5a14d7117166d97e03
teagueStockwell <tsAppDevelopment@gmail.com>
2021-01-16 01:33:00 -0800

    timegrid test fix for zulu and async timer

--- test/screens/units/timeGrid_test.dart
+++ test/screens/units/timeGrid_test.dart
@@ -36,55 +36,11 @@ void main() {
     final zJJJ = DateFormat('DDD').format(znow);
     final zHHMMSS = DateFormat('HH:mm:ss').format(znow);
 
-    expect(find.text(lYYYYMMDD), findsOneWidget);
-    expect(find.text(lJJJ), findsOneWidget);
-    expect(find.text(lHHMMSS), findsOneWidget);
-    expect(find.text(zYYYYMMDD), findsOneWidget);
-    expect(find.text(zJJJ), findsOneWidget);
-    expect(find.text(zHHMMSS), findsOneWidget);
-  });
-
-  testWidgets('given a timegird, after 1 second, then it tick',
-      (WidgetTester wt) async {
-    //given
-    final test = MaterialApp(home: Scaffold(body: TimeGrid()));
-
-    //when
-    await wt.pumpWidget(test);
-
-    final now = DateTime.now().toLocal();
-    final znow = DateTime.now().toUtc();
-    final lYYYYMMDD = DateFormat('yyyy MM dd').format(now);
-    final lJJJ = DateFormat('DDD').format(now);
-    final lHHMMSS = DateFormat('HH:mm:ss').format(now);
-    final zYYYYMMDD = DateFormat('yyyy MM dd').format(znow);
-    final zJJJ = DateFormat('DDD').format(znow);
-    final zHHMMSS = DateFormat('HH:mm:ss').format(znow);
-
-    expect(find.text(lYYYYMMDD), findsOneWidget);
-    expect(find.text(lJJJ), findsOneWidget);
-    expect(find.text(lHHMMSS), findsOneWidget);
-    expect(find.text(zYYYYMMDD), findsOneWidget);
-    expect(find.text(zJJJ), findsOneWidget);
-    expect(find.text(zHHMMSS), findsOneWidget);
-
-    //then
-    await wt.pump(Duration(seconds: 1));
-
-    final now1 = DateTime.now().toLocal();
-    final znow1 = DateTime.now().toUtc();
-    final lYYYYMMDD1 = DateFormat('yyyy MM dd').format(now1);
-    final lJJJ1 = DateFormat('DDD').format(now1);
-    final lHHMMSS1 = DateFormat('HH:mm:ss').format(now1);
-    final zYYYYMMDD1 = DateFormat('yyyy MM dd').format(znow1);
-    final zJJJ1 = DateFormat('DDD').format(znow1);
-    final zHHMMSS1 = DateFormat('HH:mm:ss').format(znow1);
-
-    expect(find.text(lYYYYMMDD1), findsOneWidget);
-    expect(find.text(lJJJ1), findsOneWidget);
-    expect(find.text(lHHMMSS1), findsOneWidget);
-    expect(find.text(zYYYYMMDD1), findsOneWidget);
-    expect(find.text(zJJJ1), findsOneWidget);
-    expect(find.text(zHHMMSS1), findsOneWidget);
+    expect(find.text(lYYYYMMDD), findsWidgets);
+    expect(find.text(lJJJ), findsWidgets);
+    expect(find.text(lHHMMSS), findsWidgets);
+    expect(find.text(zYYYYMMDD), findsWidgets);
+    expect(find.text(zJJJ), findsWidgets);
+    expect(find.text(zHHMMSS), findsWidgets);
   });
 }
