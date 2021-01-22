bb7be2438d33d6bd15596dc15566f13c05b7702e
teagueStockwell <tsAppDevelopment@gmail.com>
2021-01-22 12:31:37 -0800

    comment fire

--- lib/backend/services.dart
+++ lib/backend/services.dart
@@ -1,19 +1,23 @@
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'models/aircraft.dart';
 import 'models/glossary.dart';
 import 'models/home_model.dart';
 import 'models/more_op.dart';
+import 'package:http/http.dart' as http;
 
 class Services{
 
 Future<HomeModel> getHomeModel() async {
-  await Firebase.initializeApp();
-  
-  DocumentSnapshot qs = await FirebaseFirestore.instance
-      .collection('general')
-      .doc('general')
-      .get();
+  await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+
+  // await Firebase.initializeApp();
+  // DocumentSnapshot qs = await FirebaseFirestore.instance
+  //     .collection('general')
+  //     .doc('general')
+  //     .get();
 
   return HomeModel(
     welcome: Glossary(qs['welcometitle'], qs['welcomebody']),
@@ -23,31 +27,39 @@ Future<HomeModel> getHomeModel() async {
 
 Future<List<Aircraft>> getAirs() async {
   List<Aircraft> ret = [];
-  QuerySnapshot qs = await FirebaseFirestore.instance.collection('mds').get();
 
-  qs.docs.forEach((v) {
-    ret.add(Aircraft(
-        name: v.get('name'),
-        fs0: v.get('fs0'),
-        fs1: v.get('fs1'),
-        mom0: v.get('mom0'),
-        mom1: v.get('mom1'),
-        weight0: v.get('weight0'),
-        weight1: v.get('weight1'),
-        simplemom: v.get('simplemom'),
-        lemac: v.get('lemac'),
-        mac: v.get('mac'),
-        cargomaxweight: v.get('cargomaxweight'),
-        tanknames: v.get('tanknames'),
-        tankmoms: v.get('tankmoms'),
-        tankweights: v.get('tankweights'),
-        titles: v.get('titles'),
-        bodys: v.get('bodys'),
-        cargonames: v.get('cargonames'),
-        cargoweights: v.get('cargoweights'),
-        cargomoms: v.get('cargomoms'),
-        configstrings: v.get('configs')));
-  });
-  return ret;
-}
+  final response =  await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+  // QuerySnapshot qs = await FirebaseFirestore.instance.collection('mds').get();
+
+  if(response.statusCode ==200){
+    String airs = jsonDecode(response.body);
+
+    //for each air in airs, add air to ret
+
+    qs.docs.forEach((v) {
+      ret.add(Aircraft(
+          name: v.get('name'),
+          fs0: v.get('fs0'),
+          fs1: v.get('fs1'),
+          mom0: v.get('mom0'),
+          mom1: v.get('mom1'),
+          weight0: v.get('weight0'),
+          weight1: v.get('weight1'),
+          simplemom: v.get('simplemom'),
+          lemac: v.get('lemac'),
+          mac: v.get('mac'),
+          cargomaxweight: v.get('cargomaxweight'),
+          tanknames: v.get('tanknames'),
+          tankmoms: v.get('tankmoms'),
+          tankweights: v.get('tankweights'),
+          titles: v.get('titles'),
+          bodys: v.get('bodys'),
+          cargonames: v.get('cargonames'),
+          cargoweights: v.get('cargoweights'),
+          cargomoms: v.get('cargomoms'),
+          configstrings: v.get('configs')));
+    });
+    return ret;
+  }
 }
