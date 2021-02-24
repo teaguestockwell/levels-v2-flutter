9ad23dd219da7406b4a53e9fa0cc8787beeefbaf
teagueStockwell <tsAppDevelopment@gmail.com>
2021-02-23 17:16:12 -0800

    gitignore android && ios

--- lib/widgets/layout/div.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../../backend/const.dart';
-
-class Div extends StatelessWidget {
-  @override
-  Widget build(_){
-    return Divider(color: Const.divColor,thickness: Const.divThickness);
-  }
-}
