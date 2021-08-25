f405ab7dbb4a0f0669a09b5d63ecf15748993974
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-24 21:05:04 -0700

    Add e2e compose with api

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -7,16 +7,25 @@ import 'isolate.dart';
 class UserService{
 
 Future<General> getGeneral() async {
-  final res =  await get(baseurl + 'general');
-  return General.fromJson(
-    await decodeJsonIsolate<Map<String,dynamic>>(res.body)
-  );
+
+  return General.fromJson({
+    'role': 6,
+    'name': 'App Dev',
+    'names': ['Help/Contact', 'About Developer'],
+    'body': 'Please be reminded that this app is intended for reference, and education purposes only. While careful consideration has been taken creating this app, we do not warrant, represent or guarantee that the material published on this app, as well as the calculations made, are in all respects accurate, complete or current. To the extent permitted by law, we exclude any liability, including any liability for negligence,  for loss, damage, or bodily injury arising from the reliance on material and calculations made in this application. It is your sole responsibility to make sure all proper measures are taken to ensure safety.',
+    'iconDatas': [59478, 57360], // flutter material icon data
+    'urls': [
+      'https://forms.gle/Bbqvubn6gwC6fRnc8',
+      'https://tsappdevelopment.github.io/hellohtml/',
+    ],
+  });
 }
 
 Future<List<Aircraft>> getAirs() async {
-  final res =  await get(baseurl + 'aircraft');
-  final jsonMap = await decodeJsonIsolate<List<dynamic>>(res.body);
-  return jsonMap.map((air){
+  final res =  await get(baseurl + 'aircraft/deep');
+  final jsonMap = await decodeJsonIsolate<Map<String,dynamic>>(res.body);
+  final List<dynamic> airList = jsonMap['aircrafts'];
+  return airList.map((air){
     try{
       return Aircraft.fromJson(air as Map<String,dynamic>);
     // ignore: avoid_catches_without_on_clauses
